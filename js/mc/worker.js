b1bfbbe20a8dc4041a5ba3a5c42afe8b5b01fb00
HelloWorldZTR <zhang.tingrui@qq.com>
2020-06-06 12:12:30 +0800

    Site updated: 2020-06-06 12:12:29

--- js/mc/worker.js
+++ js/mc/worker.js
@@ -1,88 +1,119 @@
 onmessage = function (message) {
     var data = message.data;
+
     var height = Number(data.h);
     var width = Number(data.w);
     var idt = data.idt;
     var blocks = data.blc;
-    var list = new Array();
+    var render_level = data.render_level;
+
+    var list = [];
     var bp = "顺序：自下而上，自左至右\n";
-    var row = new Array();
+    var row = [];
     this.console.log(data);
     for (var y = 0; y < height; y++) {
-        if(y%10==0) {
-            var percent = y/height*100;
+        if (y % 10 == 0) {
+            var percent = y / height * 100;
             this.postMessage({
                 terminate: false,
                 per: percent
             });
-            this.console.log('当前进度:'+percent+'%')
+            this.console.log('当前进度:' + percent + '%');
         }
-        var info = "第" + (height - y) + "行: ";
-        var col = new Array();
-        var cnt = 0;
-        var last;
+        var col = [];
         for (var x = 0; x < width; x++) {
             var pixel = getPixelXY(idt, x, y);
             var diff = 10000000000;
-            var best = 0;
-            for (var i = 0; i < blocks.length; i++) {
-                var tempRGB = blocks[i].rgb;
-                var r1=pixel[0], g1 = pixel[1], b1 = pixel[2];
-                var r2=tempRGB[0], g2=tempRGB[1],b2 = tempRGB[2];
-                var rmean = (r1+r2)/2;
-                var deltaR = r1-r2,
-                    deltaG = g1-g2,
-                    deltaB = b1-b2;
-                var tempDiff = Math.sqrt(
-                    (2+rmean/256)*(deltaR*deltaR)
-                    +
-                    4*(deltaG*deltaG)
-                    +
-                    (2+(255-rmean)/256)*(deltaB*deltaB)
+            var best = [];
+            if (render_level == 1) {
+                for (var i = 0; i < blocks.length; i++) {
+                    var rgb = blocks[i].rgb;
+                    var r1 = pixel[0],
+                        g1 = pixel[1],
+                        b1 = pixel[2];
+                    var r2 = rgb[0],
+                        g2 = rgb[1],
+                        b2 = rgb[2];
+                    var rmean = (r1 + r2) / 2;
+                    var deltaR = r1 - r2,
+                        deltaG = g1 - g2,
+                        deltaB = b1 - b2;
+                    var tempDiff = Math.sqrt(
+                        (2 + rmean / 256) * (deltaR * deltaR) +
+                        4 * (deltaG * deltaG) +
+                        (2 + (255 - rmean) / 256) * (deltaB * deltaB)
                     );
-                // var tempDiff = Math.abs(tempRGB[0] - pixel[0]) + Math.abs(tempRGB[1] - pixel[1]) + Math.abs(tempRGB[2] - pixel[2]);
-                if (tempDiff < diff) {
-                    diff = tempDiff;
-                    best = i;
+                    if (tempDiff < diff) {
+                        best = [i];
+                        diff = tempDiff;
+                    }
+                }
+            } else if (render_level == 2) {
+                for (var i = 0; i < blocks.length; i++) {
+                    for (var j = 0; j < blocks.length; j++) {
+                        for (var a = 0; a < blocks.length; a++) {
+                            for (var b = 0; b < blocks.length; b++) {
+                                var avgR = (blocks[i].rgb[0] + blocks[j].rgb[0] + blocks[a].rgb[0] + blocks[b].rgb[0]) / 4;
+                                var avgG = (blocks[i].rgb[1] + blocks[j].rgb[1] + blocks[a].rgb[1] + blocks[b].rgb[1]) / 4;
+                                var avgB = (blocks[i].rgb[2] + blocks[j].rgb[2] + blocks[a].rgb[2] + blocks[b].rgb[2]) / 4;
+                                var r1 = pixel[0],
+                                    g1 = pixel[1],
+                                    b1 = pixel[2];
+                                var r2 = avgR,
+                                    g2 = avgG,
+                                    b2 = avgB;
+                                var rmean = (r1 + r2) / 2;
+                                var deltaR = r1 - r2,
+                                    deltaG = g1 - g2,
+                                    deltaB = b1 - b2;
+                                var tempDiff = Math.sqrt(
+                                    (2 + rmean / 256) * (deltaR * deltaR) +
+                                    4 * (deltaG * deltaG) +
+                                    (2 + (255 - rmean) / 256) * (deltaB * deltaB)
+                                );
+                                if (tempDiff < diff) {
+                                    best = [i, j, a, b];
+                                    diff = tempDiff;
+                                }
+                            }
+                        }
+                    }
                 }
             }
-            list[blocks[best].id] = list[blocks[best].id] == null ? 0 : list[blocks[best].id] + 1;
-            col[x] = blocks[best].id;
-            if (last == null) {
-                cnt++;
-                last = best;
-            }
-            else {
-                if (last == best) {
-                    cnt++;
-                }
-                else {//best != last
-                    info += cnt + "*" + blocks[last].name + " ";
-                    cnt = 1;
-                    last = best;
-                }
-                if (x == width - 1) {
-                    info += cnt + "*" + blocks[last].name + " ";
-                }
-            }
+            col[x] = best;
+            // if (last == null) {
+            //     cnt++;
+            //     last = best;
+            // } else {
+            //     if (last == best) {
+            //         cnt++;
+            //     } else { //best != last
+            //         info += cnt + "*" + blocks[last].name + " ";
+            //         cnt = 1;
+            //         last = best;
+            //     }
+            //     if (x == width - 1) {
+            //         info += cnt + "*" + blocks[last].name + " ";
+            //     }
+            // }
         }
         row[y] = col;
-        // console.log(info);
-        bp += info + "\n";
     }
     this.postMessage({
         terminate: true,
         row: row,
         bp: bp,
         list: list
-    })
+    });
 }
 
 // 魔法®
 function getPixel(imgData, index) {
-    var i = index * 4, d = imgData.data;
+    var i = index * 4,
+        d = imgData.data;
     return [d[i], d[i + 1], d[i + 2], d[i + 3]] // [R,G,B,A]
 }
+
 function getPixelXY(imgData, x, y) {
     return getPixel(imgData, y * imgData.width + x);
 }
