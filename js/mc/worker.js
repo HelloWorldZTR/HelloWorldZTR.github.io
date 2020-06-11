7c51f95b8cd4c8b574493d104fc90d8b7f7c9155
HelloWorldZTR <zhang.tingrui@qq.com>
2020-06-11 19:12:49 +0800

    Site updated: 2020-06-11 19:12:49

--- js/mc/worker.js
+++ js/mc/worker.js
@@ -6,13 +6,30 @@ onmessage = function (message) {
     var idt = data.idt;
     var blocks = data.blc;
     var render_level = data.render_level;
+    var root_position = [0, 0, 4];
 
+    var mcfunction = "";
     var list = [];
-    var bp = "顺序：自下而上，自左至右\n";
+    var bp = "顺序：自下而上，自左至右；即第一行为最上面一行\n";
     var row = [];
-    this.console.log(data);
+    //this.console.log(data);
+
+    var cmd_header = '/summon minecraft:falling_block ~ ~1 ~ {id: "minecraft:falling_block",Time: 1,DropItem: 0, Block: "minecraft:command_block", Passengers: [{'
+    var cmd_part = 'id: "minecraft:falling_block",Time: 1,DropItem: 0,Block: "minecraft:command_block",Passengers: [{\n';
+    var cmd_last = 'id: "minecraft:falling_block",Time: 1,DropItem: 0,Block: "minecraft:redstone_block"\n    }],\n';
+
+    var data_part_header = 'TileEntityData: {auto: 1,Command: "';
+    var data_part_end = '"}}],';
+    var data_end_header  = 'TileEntityData: {auto: 1,Command: "';
+    var data_end_end = '"}}]}';
+
+    mcfunction = cmd_header;
+    var mcfunction_end = data_end_end;
+
+    var mcfunc = "";
+
     for (var y = 0; y < height; y++) {
-        if (y % 10 == 0) {
+        if (y % 5 == 0) {
             var percent = y / height * 100;
             this.postMessage({
                 terminate: false,
@@ -21,10 +38,16 @@ onmessage = function (message) {
             this.console.log('当前进度:' + percent + '%');
         }
         var col = [];
+        var temp_col = [];
+        for (var i = 0; i < render_level; i++) {
+            temp_col[i] = [];
+        }
         for (var x = 0; x < width; x++) {
             var pixel = getPixelXY(idt, x, y);
             var diff = 10000000000;
             var best = [];
+            //TODO 效率提升
+            //TODO list 和 蓝图
             if (render_level == 1) {
                 for (var i = 0; i < blocks.length; i++) {
                     var rgb = blocks[i].rgb;
@@ -81,29 +104,78 @@ onmessage = function (message) {
                 }
             }
             col[x] = best;
-            // if (last == null) {
-            //     cnt++;
-            //     last = best;
-            // } else {
-            //     if (last == best) {
-            //         cnt++;
-            //     } else { //best != last
-            //         info += cnt + "*" + blocks[last].name + " ";
-            //         cnt = 1;
-            //         last = best;
-            //     }
-            //     if (x == width - 1) {
-            //         info += cnt + "*" + blocks[last].name + " ";
-            //     }
-            // }
+            if (render_level == 1) {
+                temp_col[0].push(best[0]);
+            } else if (render_level == 2) {
+                temp_col[0].push(best[0]), temp_col[0].push(best[1]);
+                temp_col[1].push(best[2]), temp_col[1].push(best[3]);
+            }
+            for (var i = 0; i < best.length; i++) {
+                list[blocks[best[i]].id] = list[blocks[best[i]].id] == null ? 1 : list[blocks[best[i]].id] + 1;
+            }
         }
         row[y] = col;
+        //this.console.log(temp_col);
+        //this.console.log(temp_col[0])
+        //蓝图
+
+        for (var i = 0; i < temp_col.length; i++) {
+            var temp_row = temp_col[i];
+            var last = -1;
+            var cnt = 0;
+            var info = "第" + (y * render_level + i + 1) + "行\t";
+            for (var j = 0; j < temp_row.length; j++) {
+                var id = temp_row[j];
+                //this.console.log(id);
+                var yx = root_position[0] + y * render_level + i;
+                var xx = root_position[0] + j;
+                var zx = root_position[2];
+
+                var cmd = "setblock " + xx + " " + zx + " " + yx + " " + blocks[id].mcn;
+                mcfunc+= cmd+"\n";
+                
+                if(j==temp_row.length-1 && i==temp_col.length-1 && x==width-1 && y==height-1){
+                    mcfunction += cmd_last;
+                }
+                else {
+                    mcfunction += cmd_part;
+                }
+
+                if(j==0 && i==0 && y==0 && x==0) {
+                    mcfunction_end = data_end_header + cmd + mcfunction_end;
+                }
+                else {
+                    mcfunction_end = data_part_header + cmd + data_part_end + mcfunction_end;
+                }
+
+                if (last == -1) {
+                    last = id;
+                    cnt = 1;
+                } else {
+                    if (last == id) {
+                        cnt++;
+                    } else if (last != id) { //last != id
+                        info += blocks[last].name + "*" + cnt + "\t";
+                        last = id;
+                        cnt = 1;
+                    }
+                    if (j == temp_row.length - 1) {
+                        info += blocks[last].name + "*" + cnt + "\t";
+                    }
+                }
+            }
+            bp += info + "\n";
+        }
     }
+    mcfunction += mcfunction_end;
+    this.console.log(mcfunction);
+    this.console.log(mcfunc);
     this.postMessage({
         terminate: true,
         row: row,
         bp: bp,
-        list: list
+        list: list,
+        mcfunction: mcfunc
     });
 }
 
